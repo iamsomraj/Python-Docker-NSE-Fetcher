e43427c1a107908bb784115945bfba36e553d915
RohanAlmighty <rohanduttaperfect@gmail.com>
2024-04-27 21:41:58 +0000

    👌 IMPROVE: Refactor Quote App

--- quote/app/main.py
+++ quote/app/main.py
@@ -14,47 +14,94 @@ app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
-    return {"Message": "Hello World"}
+async def read_root() -> Dict[str, str]:
+    return {"message": "Hello Google Finance"}
+
+
+async def get_price(symbol: str, exchange: str) -> Dict[str, Union[str, None]]:
+    url = f"{GOOGLE_FINANCE}{symbol}:{exchange}"
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            response.raise_for_status()
+    except httpx.HTTPError as e:
+        if e.response.status_code == 404:
+            raise HTTPException(status_code=404, detail="Symbol not found")
+        else:
+            raise HTTPException(
+                status_code=500,
+                detail="Failed to fetch data from Google Finance",
+            )
+    except httpx.RequestError:
+        raise HTTPException(
+            status_code=500, detail="Failed to connect to Google Finance"
+        )
+
+    soup = BeautifulSoup(response.text, "lxml")
+
+    try:
+        name = soup.find("div", class_="zzDege").text
+        price = soup.find("div", class_="YMlKec fxKbKc").text
+    except AttributeError:
+        raise HTTPException(
+            status_code=500,
+            detail="Failed to parse response from Google Finance. HTML structure might have changed.",
+        )
+
+    return {"name": name, "price": price}
 
 
 @app.get("/index/{symbol}")
 async def get_index_price(symbol: str) -> Dict[str, Union[str, None]]:
-    url = GOOGLE_FINANCE + symbol + f":{EXCHANGE['index']}"
-
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-
-    if response.status_code != 200:
-        raise HTTPException(
-            status_code=response.status_code,
-            detail="Failed to fetch data from Google Finance",
-        )
-
-    soup = BeautifulSoup(response.text, "lxml")
-
-    try:
-        name = soup.find("div", class_="zzDege").text
-        price = soup.find("div", class_="YMlKec fxKbKc").text
-    except AttributeError:
-        raise HTTPException(
-            status_code=404, detail="Symbol not found or HTML structure changed"
-        )
-
-    return {"name": name, "price": price}
+    return await get_price(symbol, EXCHANGE["index"])
 
 
 @app.get("/nse/{symbol}")
-async def get_index_price(symbol: str) -> Dict[str, Union[str, None]]:
-    url = GOOGLE_FINANCE + symbol + f":{EXCHANGE['nse']}"
+async def get_nse_price(symbol: str) -> Dict[str, Union[str, None]]:
+    return await get_price(symbol, EXCHANGE["nse"])
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
 
-    if response.status_code != 200:
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
+from typing import Dict, Union
+from fastapi import FastAPI, HTTPException
+import uvicorn
+import httpx
+from bs4 import BeautifulSoup
+
+GOOGLE_FINANCE = "https://www.google.com/finance/quote/"
+EXCHANGE = {
+    "index": "INDEXNSE",
+    "nse": "NSE",
+}
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root() -> Dict[str, str]:
+    return {"message": "Hello Google Finance"}
+
+
+async def get_price(symbol: str, exchange: str) -> Dict[str, Union[str, None]]:
+    url = f"{GOOGLE_FINANCE}{symbol}:{exchange}"
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            response.raise_for_status()
+    except httpx.HTTPError as e:
+        if e.response.status_code == 404:
+            raise HTTPException(status_code=404, detail="Symbol not found")
+        else:
+            raise HTTPException(
+                status_code=500,
+                detail="Failed to fetch data from Google Finance",
+            )
+    except httpx.RequestError:
         raise HTTPException(
-            status_code=response.status_code,
-            detail="Failed to fetch data from Google Finance",
+            status_code=500, detail="Failed to connect to Google Finance"
         )
 
     soup = BeautifulSoup(response.text, "lxml")
@@ -64,10 +111,21 @@ async def get_index_price(symbol: str) -> Dict[str, Union[str, None]]:
         price = soup.find("div", class_="YMlKec fxKbKc").text
     except AttributeError:
         raise HTTPException(
-            status_code=404, detail="Symbol not found or HTML structure changed"
+            status_code=500,
+            detail="Failed to parse response from Google Finance. HTML structure might have changed.",
         )
 
     return {"name": name, "price": price}
+
+
+@app.get("/index/{symbol}")
+async def get_index_price(symbol: str) -> Dict[str, Union[str, None]]:
+    return await get_price(symbol, EXCHANGE["index"])
+
+
+@app.get("/nse/{symbol}")
+async def get_nse_price(symbol: str) -> Dict[str, Union[str, None]]:
+    return await get_price(symbol, EXCHANGE["nse"])
 
 
 if __name__ == "__main__":
